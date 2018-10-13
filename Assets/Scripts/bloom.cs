497518e004dae4e5e5809ff249dbc5d89e93c10f
Carl <carlospidla@gmail.com>
2018-10-13 17:08:09 -0400

    access values from knobs with float "potValue"

---
+++ Assets/Scripts/bloom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uniduino.Examples;
+using UnityEngine.PostProcessing;
+
+public class bloom : MonoBehaviour {
+
+	// Use this for initialization
+	public float bloomIntensity;
+	float pot4Input;
+	public float pot4Value;
+	public int size = 715;
+	public PostProcessingProfile ppProfile;
+
+
+	void Start () {
+		var bloom = ppProfile.bloom.settings;	
+
+
+	}
+	
+	void Update () {
+
+		pot4Input = GameObject.Find("Analog Read").GetComponent<AnalogReadPots>().pinValueA3;
+
+		pot4Value = pot4Input/102.3f;
+
+		//transform.localScale = new Vector3(150 + pot1Value * size, 150 + pot1Value * size, 150 +pot1Value * size);
+
+
+		var bloom = ppProfile.bloom.settings;
+		bloom.bloom.intensity = pot4Value * bloomIntensity;
+		ppProfile.bloom.settings = bloom;
+
+		//transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+	}
+}
---
+++ Assets/Scripts/set2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uniduino.Examples;
+
+public class set2 : MonoBehaviour {
+
+	// Use this for initialization
+
+	float pot2Input;
+	public float pot2Value;
+
+
+
+	void Start () {
+
+
+
+	}
+	
+	void Update () {
+
+		pot2Input = GameObject.Find("Analog Read").GetComponent<AnalogReadPots>().pinValueA1;
+
+		pot2Value = pot2Input/102.3f;
+
+		transform.localScale = new Vector3(0.2f + pot2Value * 0.4f, 0.2f + pot2Value * 0.4f, 0.2f +pot2Value * 0.4f);
+
+		//print ("pot Value " + pot2Value);
+
+		//transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * growFactor;s
+	}
+}
